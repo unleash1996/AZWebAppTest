146d359b0bf58ddcfdc09ef1f5eb6bc7628b67c2
abrah <abrah@ELSHADDAI>
2024-03-23 22:08:49 -0500

    Add project files.

---
+++ AZWebAppTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AZWebAppTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
